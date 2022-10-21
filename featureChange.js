03426f3024f0f52b666e9bb6395cb82eaf593417
Avik Seth <avik.seth@grazitti.local>
2022-10-21 11:58:47 +0530

    commit 4

--- featureChange.js
+++ featureChange.js
@@ -2,6 +2,7 @@ function sampleFunction(some_params){
     console.log('I am feature changes');
     console.log("change 2");
     console.log("Change 3");
+    console.log("Change 4");
     return
 }
 
